e3793464b137a00887ba85489cab0674dd268570
bwv-hoang-pt <hoang.pt@briswell-vn.com>
2025-10-03 11:07:29 +0700

    Migrate database

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,6 +3,6 @@ import { defineNitroConfig } from "nitropack/config"
 // https://nitro.build/config
 export default defineNitroConfig({
   compatibilityDate: "latest",
-  srcDir: "server",
+  srcDir: "src",
   imports: false
 });
